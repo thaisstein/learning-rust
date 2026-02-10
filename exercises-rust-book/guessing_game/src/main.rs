69afe520f53161322796916cfbc1b61548c74b44
thaisstein <tatastein88@gmail.com>
2026-02-10 11:14:37 -0300

    guessing game

--- exercises-rust-book/guessing_game/src/main.rs
+++ exercises-rust-book/guessing_game/src/main.rs
@@ -1,15 +1,39 @@
-use std::io; // obtain user
+use std::io; // obtain user input
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
+    let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("Guess the number!");
+    loop {
 
-    println!("Please input your guess.");
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
+        let mut guess = String::new(); // mutable variable
+        // new is a function associated with String
 
-    io::stdin()
-    .read_line(&mut guess)
-    .expect("Failed to read line");
+        io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Enter a valid number");
+                continue;
+            }
+        };
+
+        println!("You guessed: {}.", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                 println!("You win");
+                 break;
+            }
+        }
+
+    }
 }
