a9a0b8a28b6053d2814c1ac792b286d2bc91c84b
thaisstein <tatastein88@gmail.com>
2026-02-13 15:30:53 -0300

    arrays and tuples

--- exercises-rust-book/compound_types/src/main.rs
+++ exercises-rust-book/compound_types/src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 fn main() {
     // tuple
     let tup = (500, 6.4, 1);
@@ -10,4 +12,19 @@ fn main() {
     let b: [i32; 5] = [1, 2, 3, 4, 5] ;// type, # of elements
     let c = [3;5]; //his creates an array [3, 3, 3, 3, 3]
     println!("element {}", months[4]);
+
+    let mut index = String::new(); //for input
+
+    io::stdin()
+    .read_line(&mut index)
+    .expect("Failed to read line");
+
+    let index: usize = index //transforms in inter
+    .trim()
+    .parse()
+    .expect("Index entered was not a number");
+
+    let element = months[index];
+
+    println!("Month {} is: {}", index + 1, element);
 }
