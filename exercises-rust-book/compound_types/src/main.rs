ab9d0b507ae394e62dd1ae99c881d8d91224846e
thaisstein <tatastein88@gmail.com>
2026-02-13 13:56:28 -0300

    new

---
+++ exercises-rust-book/compound_types/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    // tuple
+    let tup = (500, 6.4, 1);
+    let(x, y, z) = tup;
+    println!("{}", tup.2);
+
+    // array
+    let a = [1, 2, 3, 4, 5];
+    let months = ["jan", "fev", "march", "apr", "may", "june", "july", "aug", "sept", "oct", "nov", "dec"];
+    let b: [i32; 5] = [1, 2, 3, 4, 5] // type, # of elements
+    let c = [3;5]; //his creates an array [3, 3, 3, 3, 3]
+}
