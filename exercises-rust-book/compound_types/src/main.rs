dbe8ac0044e22cdccd835635cf5cb301b1dd55db
thaisstein <tatastein88@gmail.com>
2026-02-13 15:19:15 -0300

    wip

--- exercises-rust-book/compound_types/src/main.rs
+++ exercises-rust-book/compound_types/src/main.rs
@@ -7,6 +7,7 @@ fn main() {
     // array
     let a = [1, 2, 3, 4, 5];
     let months = ["jan", "fev", "march", "apr", "may", "june", "july", "aug", "sept", "oct", "nov", "dec"];
-    let b: [i32; 5] = [1, 2, 3, 4, 5] // type, # of elements
+    let b: [i32; 5] = [1, 2, 3, 4, 5] ;// type, # of elements
     let c = [3;5]; //his creates an array [3, 3, 3, 3, 3]
+    println!("element {}", months[4]);
 }
