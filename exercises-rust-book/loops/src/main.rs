6c02500f74bb75cbf53149b8098a627dd61e9d62
thaisstein <tatastein88@gmail.com>
2026-02-19 09:18:34 -0300

    wip

--- exercises-rust-book/loops/src/main.rs
+++ exercises-rust-book/loops/src/main.rs
@@ -1,23 +1,17 @@
 fn main() {
-    let mut counter = 0;
-
-
-    'counting_up: loop {
-        println!("count = {counter}");
-        let mut remaining = 10;
-
-        loop {
-            println!("remaining = {remaining}");
-            if remaining == 9 {
-                break;
-            }
-            if counter == 2 {
-                break 'counting_up;
-            }
-            remaining -= 1;
-        }
-
-        counter += 1;
+    let a = [10, 20, 30, 40, 50];
+    let b = [5;10];
+    let mut sum = 0;
+    for element in a {
+        println!("the value is: {element}");
+    } 
+    for x in b {
+        sum +=x;
     }
-    println!("End count = {counter}");
+    println!("{}", sum);
+    //rev
+    for number in (1..5).rev() {
+        println!("{}!",number);
+    }  
+    println!("liftoff!");
 }
